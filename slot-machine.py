e66a4e91e09339eb0923b7d77727fbbffd4881d5
Buğrahan Kök <bugrahankok@gmail.com>
2023-12-03 07:38:21 +0100

    Update slot-machine

--- slot-machine.py
+++ slot-machine.py
@@ -1,25 +1,64 @@
 import random 
 import time
 
-symbols = ["diamond", "gold", "silver"]
+diamond = ["diamond", "diamond", "diamond"]
+gold = ["gold", "gold", "gold"]
+symbols = ["diamond", "gold", "silver", "bronze", gold, diamond]
 balance = 1000
 
-def check_winner (winner_symbol):
-    if len(set(winner_symbol)) == 1:
-        print("3 Katı")
-    elif len(set(winner_symbol)) == 2:
-        print("2 katı")
-    else: 
-        print("zort")
-        
+def has_nested_list(lst, sublist_name):
+    return any(isinstance(item, list) and item == sublist_name for item in lst)
+
+def check_winner(winner_symbols, bet):
+    if has_nested_list(winner_symbols, diamond) >= 1:
+        print("Diamond, Diamond, Diamond \n Buğra Jackpot")
+        return
+    elif has_nested_list(winner_symbols, gold) >= 1:
+        print("Gold, Gold, Gold \nGold Jackpot")
+        return 
+    
+    diamond_count = winner_symbols.count("diamond")
+    gold_count = winner_symbols.count("gold")
+    silver_count = winner_symbols.count("silver")
+    bronze_count = winner_symbols.count("bronze")
+
+    if winner_symbols == symbols[4]:
+        print(winner_symbols)
+        print("Buğra jackpot")
+    elif gold_count == 3:
+        print(winner_symbols)
+        print("gold jackpot")
+        bet = bet * 1000
+        print(bet)
+    elif silver_count == 3:
+        print(winner_symbols)
+        print("silver jackpot")
+        bet = bet * 500
+        print(bet)
+    elif bronze_count == 3:
+        print(winner_symbols)
+        print("bronze jackpot")
+        bet = bet * 200
+        print(bet)
+    elif bronze_count == 2:
+        print(winner_symbols)
+        print("bronze pair")
+        bet = bet * 50
+        print(bet)
+    elif bronze_count == 1:
+        print(winner_symbols)
+        print("bronze win")
+        bet = bet * 30
+        print(bet)
+
+
 def slot ():
-    bet = input("Enter your bet: ")
-    print("Your bet: " + bet)
+    bet = int(input("Enter your bet: "))
+    print(f"Your bet: {bet}")
     print("Machine started!")
     time.sleep(2)
-    winner_symbol = random.choices(symbols, weights = (1, 5, 10), k=3)
-    print(winner_symbol)
+    winner_symbols = random.choices(symbols, weights = (11, 10, 10, 10, 3, 1), k=3)
     
-    check_winner(winner_symbol)
+    check_winner(winner_symbols, bet)
 
 slot()
